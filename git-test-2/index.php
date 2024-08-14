f31c8ae6cc9b08e866943c139b1fc310e43caad5
savitrisma <rismasavitri51@smp.belajar.id>
2024-08-14 14:49:02 +0700

    menambahkan list pada file index.php

--- git-test-2/index.php
+++ git-test-2/index.php
@@ -7,11 +7,16 @@
     <title>Document</title>
 </head>
 
-<body>
+<>
     <h1>hello world!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut consequuntur repellat sunt dicta maiores vero, minus
         enim nemo nobis voluptate ipsum voluptas asperiores iste eaque repudiandae blanditiis excepturi velit pariatur.z
     </p>
-</body>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
+</>
 
 </html>
