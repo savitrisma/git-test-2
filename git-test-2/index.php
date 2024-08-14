9b04d54cc2466e5cd3b46a44b205095b702bdf62
savitrisma <rismasavitri51@smp.belajar.id>
2024-08-14 14:41:17 +0700

    mengubah file index.php

--- git-test-2/index.php
+++ git-test-2/index.php
@@ -9,6 +9,9 @@
 
 <body>
     <h1>hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut consequuntur repellat sunt dicta maiores vero, minus
+        enim nemo nobis voluptate ipsum voluptas asperiores iste eaque repudiandae blanditiis excepturi velit pariatur.z
+    </p>
 </body>
 
 </html>
